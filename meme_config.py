6f232ae3f11b10188edb2295a0f0eb2828d6e1c9
Svapnil <svapnila@gmail.com>
2020-11-16 00:01:11 -0500

    added soundbites

--- meme_config.py
+++ meme_config.py
@@ -2,5 +2,7 @@ REGEX_TO_MEME = {
    ".*(barber|holy smokes).*" : "shoutoutbarber.mp3",
    ".*big (dog|dawg).*" : "whatupbigdawgs.mp3",
    ".*best friends?.*" : "twoprettybestfriends.mp3",
-   ".*pog.*" : "faker.mp3"
+   ".*pog.*" : "faker.mp3",
+   ".*penta.*" : "penta.mp3",
+   ".*quadra.*" : "quadra.mp3"
 }
