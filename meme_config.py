030182be3ae4f408cffad02aec3ae5c56cf0c7b0
Svapnil <svapnila@gmail.com>
2020-11-15 23:47:35 -0500

    added two best friends

--- meme_config.py
+++ meme_config.py
@@ -1,4 +1,5 @@
 REGEX_TO_MEME = {
    ".*(barber|holy smokes).*" : "shoutoutbarber.mp3",
-   ".*big (dog|dawg).*" : "whatupbigdawgs.mp3"
+   ".*big (dog|dawg).*" : "whatupbigdawgs.mp3",
+   ".*best friends?.*" : "twoprettybestfriends.mp3"
 }
