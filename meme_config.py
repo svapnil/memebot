f9e625e7991a646efa3697ea36d8d9dc2baea66c
Svapnil <svapnila@gmail.com>
2020-11-15 23:55:12 -0500

    updated with faker clip

--- meme_config.py
+++ meme_config.py
@@ -1,5 +1,6 @@
 REGEX_TO_MEME = {
    ".*(barber|holy smokes).*" : "shoutoutbarber.mp3",
    ".*big (dog|dawg).*" : "whatupbigdawgs.mp3",
-   ".*best friends?.*" : "twoprettybestfriends.mp3"
+   ".*best friends?.*" : "twoprettybestfriends.mp3",
+   ".*pog.*" : "faker.mp3"
 }
