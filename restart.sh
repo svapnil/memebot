1ba8a221204794b5c552e665848856f3c2b71cc0
Svapnil <svapnila@gmail.com>
2020-11-21 12:43:02 -0500

    added pm2 process

--- restart.sh
+++ restart.sh
@@ -1,5 +1,8 @@
 # kill current process before loading code
+echo "Killing all current PM2 processes"
 pm2 kill
+echo "Fetching and resetting from origin/master"
 sudo git fetch --all
 sudo git reset --hard origin/master
+echo "Launching Memebot process on PM2"
 pm2 start main.py --interpreter python3
