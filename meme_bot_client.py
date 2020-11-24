1d0c63e6a26738487b3431f0dc1039a1dfd9ef12
Svapnil <svapnila@gmail.com>
2020-11-24 00:00:13 -0500

    refactored into league client

--- meme_bot_client.py
+++ meme_bot_client.py
@@ -1,9 +1,9 @@
 import discord
 from meme_player import MemePlayer
 from meme_config import REGEX_TO_MEME
-import re
+from league_client import LeagueClient
 from cassiopeia import Summoner
-
+import re
 class MemeBotClient(discord.Client):
     async def on_ready(self):
         print('Logged on as', self.user)
@@ -15,62 +15,7 @@ class MemeBotClient(discord.Client):
 
 
         if re.search("/gamesummary (.*)", message.content):
-            regex_match_rule = re.compile("/gamesummary (.*)")
-            summoner_name = regex_match_rule.match(message.content).group(1)
-
-            summoner = Summoner(name=summoner_name, region="NA")
-            player_match_history = summoner.match_history()
-            participants = {}
-            
-            printMessage = "```"
-
-            printMessage += "{:>18} {:>14} {:>14} {:>15} {:>16}".format("SUMMONER", 
-                                                                    "KILLS", 
-                                                                    "DEATHS", 
-                                                                    "ASSISTS", 
-                                                                    "KDA") + "\n"
-
-
-            for blue in player_match_history[0].blue_team.participants:
-                player = player_match_history[0].blue_team.participants[blue]
-
-                participants[player] = {}
-
-                participants[player]['summoner_name'] = player.summoner.name
-                participants[player]['kills'] = player.stats.kills
-                participants[player]['deaths'] = player.stats.deaths
-                participants[player]['assists'] = player.stats.assists
-                participants[player]['KDA'] = str(round(player.stats.kda, 2))
-
-                printMessage += "{:>18} {:>14} {:>14} {:>15} {:>16}".format(participants[player]['summoner_name'],
-                                                                participants[player]['kills'],
-                                                                participants[player]['deaths'],
-                                                                participants[player]['assists'],
-                                                                participants[player]['KDA']) + "\n" 
-
-            printMessage += "----------------------------------------------------------------------------------\n"
-
-            for red in player_match_history[0].red_team.participants:
-                player = player_match_history[0].red_team.participants[red]
-
-                participants[player] = {}
-
-                participants[player]['summoner_name'] = player.summoner.name
-                participants[player]['kills'] = player.stats.kills
-                participants[player]['deaths'] = player.stats.deaths
-                participants[player]['assists'] = player.stats.assists
-                participants[player]['KDA'] = str(round(player.stats.kda, 2))
-
-                printMessage += "{:>18} {:>14} {:>14} {:>15} {:>16}".format(participants[player]['summoner_name'],
-                                                            participants[player]['kills'],
-                                                            participants[player]['deaths'],
-                                                            participants[player]['assists'],
-                                                            participants[player]['KDA']) + "\n"
-
-            printMessage += "```"
-            await message.channel.send(printMessage)
-            print("Outputting Game Summary for: ",  summoner_name)
-
+           await LeagueClient.display_game_summary(message)
 
         if re.search("/bestchamps (.*)", message.content):
             summoner_name = message.content[12:]
