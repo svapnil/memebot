42558e2ee7ffb0a0867bb3ff51132356ad37414a
Nikhil Kaul <nkaul2@ncsu.edu>
2020-11-23 23:11:16 -0500

    added game summary functionality

--- meme_bot_client.py
+++ meme_bot_client.py
@@ -13,6 +13,65 @@ class MemeBotClient(discord.Client):
         if message.author == self.user:
             return
 
+
+        if re.search("/gamesummary (.*)", message.content):
+            regex_match_rule = re.compile("/gamesummary (.*)")
+            summoner_name = regex_match_rule.match(message.content).group(1)
+
+            summoner = Summoner(name=summoner_name, region="NA")
+            player_match_history = summoner.match_history()
+            participants = {}
+            
+            printMessage = "```"
+
+            printMessage += "{:>18} {:>14} {:>14} {:>15} {:>16}".format("SUMMONER", 
+                                                                    "KILLS", 
+                                                                    "DEATHS", 
+                                                                    "ASSISTS", 
+                                                                    "KDA") + "\n"
+
+
+            for blue in player_match_history[0].blue_team.participants:
+                player = player_match_history[0].blue_team.participants[blue]
+
+                participants[player] = {}
+
+                participants[player]['summoner_name'] = player.summoner.name
+                participants[player]['kills'] = player.stats.kills
+                participants[player]['deaths'] = player.stats.deaths
+                participants[player]['assists'] = player.stats.assists
+                participants[player]['KDA'] = str(round(player.stats.kda, 2))
+
+                printMessage += "{:>18} {:>14} {:>14} {:>15} {:>16}".format(participants[player]['summoner_name'],
+                                                                participants[player]['kills'],
+                                                                participants[player]['deaths'],
+                                                                participants[player]['assists'],
+                                                                participants[player]['KDA']) + "\n" 
+
+            printMessage += "----------------------------------------------------------------------------------\n"
+
+            for red in player_match_history[0].red_team.participants:
+                player = player_match_history[0].red_team.participants[red]
+
+                participants[player] = {}
+
+                participants[player]['summoner_name'] = player.summoner.name
+                participants[player]['kills'] = player.stats.kills
+                participants[player]['deaths'] = player.stats.deaths
+                participants[player]['assists'] = player.stats.assists
+                participants[player]['KDA'] = str(round(player.stats.kda, 2))
+
+                printMessage += "{:>18} {:>14} {:>14} {:>15} {:>16}".format(participants[player]['summoner_name'],
+                                                            participants[player]['kills'],
+                                                            participants[player]['deaths'],
+                                                            participants[player]['assists'],
+                                                            participants[player]['KDA']) + "\n"
+
+            printMessage += "```"
+            await message.channel.send(printMessage)
+            print("Outputting Game Summary for: ",  summoner_name)
+
+
         if re.search("/bestchamps (.*)", message.content):
             summoner_name = message.content[12:]
             bestchamps_message ="**Best champs for " + summoner_name + ":**\n"
