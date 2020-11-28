893d37f262a02f25c9e417f9d7528d243586a3ae
Svapnil <svapnila@gmail.com>
2020-11-27 23:37:55 -0500

    added whisper dm tts

--- client/bot.py
+++ client/bot.py
@@ -1,11 +1,25 @@
 from config.bot import REGEX_TO_ACTION
-from discord import Client, File
+from discord import Client, File, DMChannel, FFmpegPCMAudio
 from discord.errors import ClientException
+from gtts import gTTS
+import asyncio
 import re
 
 class MemeBotClient(Client):
+    def __init__(self):
+        self.voice_lookup = {}    
+        super().__init__()
+
     async def on_ready(self):
         print('Logged on as', self.user)
+
+    async def on_voice_state_update(self, member, before, after):
+        # don't respond to ourselves
+        if member.id == self.user.id:
+            return
+        print("voice state update")
+        self.voice_lookup[member.id] = after.channel
+        return
 
     async def on_message(self, message):
         # don't respond to ourselves
@@ -13,6 +27,25 @@ class MemeBotClient(Client):
             return
 
         try:
+            if isinstance(message.channel, DMChannel):
+                if re.search("whisper", message.content.lower()):
+                    payload = message.content[7:]
+                    tts = gTTS(text=payload, lang="en", slow=False)
+                    tts.save("_tts.mp3")
+                    channel = self.voice_lookup.get(message.author.id)
+                    if not channel:
+                        await message.channel.send(f'Try rejoining the Discord channel and trying again my neighbor')             
+                        return
+                    voice = await channel.connect()
+                    audio = FFmpegPCMAudio("_tts.mp3")
+                    voice.play(audio)
+                    print(f'Playing whisper')
+                    print(f'Payload: {payload}')
+                    print(f'From: {message.author.name}')
+                    while(voice.is_playing()):
+                        await asyncio.sleep(1)
+                    await voice.disconnect()
+                    return
             for regex, action in REGEX_TO_ACTION.items():
                 if re.search(regex, message.content.lower()):
                     await action(message)
