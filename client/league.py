e98e575166bda0e80dfea32f800d67ca48701a60
Svapnil <svapnila@gmail.com>
2020-12-08 00:29:54 -0500

    submitted match history

--- client/league.py
+++ client/league.py
@@ -3,7 +3,11 @@ from helper.league import LeagueClientHelper
 from utils.logger import Logger
 import re
 from cassiopeia import Summoner
+from cassiopeia.data import GameMode
 
+def format_game_mode(mode : GameMode) -> str:
+    # "GameMode.aram" -> "ARAM"
+    return str(mode)[9:].upper()
 class LeagueClient:
     @staticmethod
     async def display_game_summary(message : Message) -> None:
@@ -35,3 +39,32 @@ class LeagueClient:
         await message.channel.send(bestchamps_message)
         Logger.log("Outputting Best Champ")
     
+    @staticmethod
+    async def display_match_history(message : Message) -> None:
+        summoner_name = message.content[14:]
+        summoner = Summoner(name=summoner_name, region="NA")
+        history = summoner.match_history[:5]
+        output = "{:>8} {:>14} {:>14} {:>14} {:>15} {:>16}".format(
+                                                    "MODE",
+                                                    "CHAMPION",
+                                                    "KILLS",
+                                                    "DEATHS",
+                                                    "ASSISTS",
+                                                    "KDA") + "\n" 
+        for match in history:
+            for player in match.participants:
+                if player.summoner.id == summoner.id:
+                    kills = player.stats.kills
+                    deaths = player.stats.deaths
+                    assists = player.stats.assists
+                    kda = str(round(player.stats.kda, 2))
+                    output += "{:>8} {:>14} {:>14} {:>14} {:>15} {:>16}".format(
+                                                                format_game_mode(match.mode),
+                                                                player.champion.name,
+                                                                kills,
+                                                                deaths,
+                                                                assists,
+                                                                kda) + "\n" 
+                    
+        await message.channel.send(f'**Last 5 Games for {summoner.name}:**```{output}```')
+        Logger.log("Outputting Match History")
--- config/bot.py
+++ config/bot.py
@@ -4,8 +4,10 @@ from client.meme import MemeClient
 GM_REGEX_TO_ACTION = {
     # display summary of most recent game
     "/gamesummary (.*)" : LeagueClient.display_game_summary,
-    # display best champs for a champion
+    # display best champs for a summoner
     "/bestchamps (.*)" : LeagueClient.display_best_champs,
+    # display match history for a summoner
+    "/matchhistory (.*)" : LeagueClient.display_match_history,
     # display manual of all possible regexes
     "/memehelp" : MemeClient.display_meme_help,
     # display manual for whisper help
